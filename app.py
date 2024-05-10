47f0c939b4794a5a3f949cc81b38000f52017c1d
Kuang Wen <36161062+kwen1510@users.noreply.github.com>
2024-05-11 00:24:07 +0800

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,22 @@ import json
 import os
 from annoy import AnnoyIndex
 import cohere
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
 
-st.text(st.secrets["MONGO_DB"])
+password = st.secrets["MONGO_DB"])
+
+uri = f"mongodb+srv://kwen1510:{password}@cluster0.bwtbeur.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
+# Create a new client and connect to the server
+client = MongoClient(uri, server_api=ServerApi('1'))
+
+# Send a ping to confirm a successful connection
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
 
 # Initialize Cohere client
 co = cohere.Client(os.getenv('COHERE_API_KEY'))
